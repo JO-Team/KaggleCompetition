8af0a9aad8ab263bb1cf75c4c83923da36e5f592
jason-learn <workmail.jason@foxmail.com>
2017-08-07 00:14:48 +0800

    f1

--- Instacart_Market_Basket_Analysis/src/kunV2.py
+++ Instacart_Market_Basket_Analysis/src/kunV2.py
@@ -187,113 +187,111 @@ def ka_add_groupby_features_n_vs_1(df, group_columns_list, target_columns_list, 
 path_data = '../input/'
 priors, train, orders, products, aisles, departments, sample_submission = load_data(path_data)
 
-try:
-    data = pd.read_pickle('kernel38-data.pkl')
-except:
-    # Products information ----------------------------------------------------------------
-    # add order information to priors set
-    priors_orders_detail = orders.merge(right=priors, how='inner', on='order_id')
 
-    # create new variables
-    # _user_buy_product_times: 用户是第几次购买该商品
-    priors_orders_detail.loc[:,'_user_buy_product_times'] = priors_orders_detail.groupby(['user_id', 'product_id']).cumcount() + 1
-    # _prod_tot_cnts: 该商品被购买的总次数,表明被喜欢的程度
-    # _reorder_tot_cnts_of_this_prod: 这件商品被再次购买的总次数
-    ### 我觉得下面两个很不好理解，考虑改变++++++++++++++++++++++++++
-    # _prod_order_once: 该商品被购买一次的总次数
-    # _prod_order_more_than_once: 该商品被购买一次以上的总次数
-    agg_dict = {'user_id':{'_prod_tot_cnts':'count'},
-                'reordered':{'_prod_reorder_tot_cnts':'sum'},
-                '_user_buy_product_times': {'_prod_buy_first_time_total_cnt':lambda x: sum(x==1),
-                                            '_prod_buy_second_time_total_cnt':lambda x: sum(x==2)}}
-    prd = ka_add_groupby_features_1_vs_n(priors_orders_detail, ['product_id'], agg_dict)
+# Products information ----------------------------------------------------------------
+# add order information to priors set
+priors_orders_detail = orders.merge(right=priors, how='inner', on='order_id')
 
-    # _prod_reorder_prob: 这个指标不好理解
-    # _prod_reorder_ratio: 商品复购率
-    prd['_prod_reorder_prob'] = prd._prod_buy_second_time_total_cnt / prd._prod_buy_first_time_total_cnt
-    prd['_prod_reorder_ratio'] = prd._prod_reorder_tot_cnts / prd._prod_tot_cnts
-    prd['_prod_reorder_times'] = 1 + prd._prod_reorder_tot_cnts / prd._prod_buy_first_time_total_cnt
+# create new variables
+# _user_buy_product_times: 用户是第几次购买该商品
+priors_orders_detail.loc[:,'_user_buy_product_times'] = priors_orders_detail.groupby(['user_id', 'product_id']).cumcount() + 1
+# _prod_tot_cnts: 该商品被购买的总次数,表明被喜欢的程度
+# _reorder_tot_cnts_of_this_prod: 这件商品被再次购买的总次数
+### 我觉得下面两个很不好理解，考虑改变++++++++++++++++++++++++++
+# _prod_order_once: 该商品被购买一次的总次数
+# _prod_order_more_than_once: 该商品被购买一次以上的总次数
+agg_dict = {'user_id':{'_prod_tot_cnts':'count'},
+            'reordered':{'_prod_reorder_tot_cnts':'sum'},
+            '_user_buy_product_times': {'_prod_buy_first_time_total_cnt':lambda x: sum(x==1),
+                                        '_prod_buy_second_time_total_cnt':lambda x: sum(x>=2)}}
+prd = ka_add_groupby_features_1_vs_n(priors_orders_detail, ['product_id'], agg_dict)
 
-    # User Part
-    # _user_total_orders: 用户的总订单数
-    # 可以考虑加入其它统计指标++++++++++++++++++++++++++
-    # _user_sum_days_since_prior_order: 距离上次购买时间(和),这个只能在orders表里面计算，priors_orders_detail不是在order level上面unique
-    # _user_mean_days_since_prior_order: 距离上次购买时间(均值)
-    agg_dict_2 = {'order_number':{'_user_total_orders':'max'},
+# _prod_reorder_prob: 这个指标不好理解
+# _prod_reorder_ratio: 商品复购率
+prd['_prod_reorder_prob'] = prd._prod_buy_second_time_total_cnt / prd._prod_buy_first_time_total_cnt
+prd['_prod_reorder_ratio'] = prd._prod_reorder_tot_cnts / prd._prod_tot_cnts
+prd['_prod_reorder_times'] = 1 + prd._prod_reorder_tot_cnts / prd._prod_buy_first_time_total_cnt
+
+# User Part
+# _user_total_orders: 用户的总订单数
+# 可以考虑加入其它统计指标++++++++++++++++++++++++++
+# _user_sum_days_since_prior_order: 距离上次购买时间(和),这个只能在orders表里面计算，priors_orders_detail不是在order level上面unique
+# _user_mean_days_since_prior_order: 距离上次购买时间(均值)
+agg_dict_2 = {'order_number':{'_user_total_orders':'max'},
 
 
-                  'days_since_prior_order':{'_user_sum_days_since_prior_order':'sum',
-                                            '_user_mean_days_since_prior_order': 'mean'}}
-    users = ka_add_groupby_features_1_vs_n(orders[orders.eval_set == 'prior'], ['user_id'], agg_dict_2)
-    #总购物车数量
-    users['nb_orders'] = orders.groupby('user_id').size().astype(np.int16)
+              'days_since_prior_order':{'_user_sum_days_since_prior_order':'sum',
+                                        '_user_mean_days_since_prior_order': 'mean'}}
+users = ka_add_groupby_features_1_vs_n(orders[orders.eval_set == 'prior'], ['user_id'], agg_dict_2)
+#总购物车数量
+users['nb_orders'] = orders.groupby('user_id').size().astype(np.int16)
 
-    # _user_reorder_ratio: reorder的总次数 / 第一单后买后的总次数
-    # _user_total_products: 用户购买的总商品数
-    # _user_distinct_products: 用户购买的unique商品数
-    agg_dict_3 = {'reordered':
-                  {'_user_reorder_ratio':
-                   lambda x: sum(priors_orders_detail.ix[x.index,'reordered']==1)/
-                             sum(priors_orders_detail.ix[x.index,'order_number'] > 1)},
-                  'product_id':{'_user_total_products':'count',
-                                '_user_distinct_products': lambda x: x.nunique()}}
-    #us = ka_add_groupby_features_1_vs_n(priors_orders_detail, ['user_id'], agg_dict_3)
-    ##########################################################################################
-    us = pd.concat([
+# _user_reorder_ratio: reorder的总次数 / 第一单后买后的总次数
+# _user_total_products: 用户购买的总商品数
+# _user_distinct_products: 用户购买的unique商品数
+agg_dict_3 = {'reordered':
+              {'_user_reorder_ratio':
+               lambda x: sum(priors_orders_detail.ix[x.index,'reordered']==1)/
+                         sum(priors_orders_detail.ix[x.index,'order_number'] > 1)},
+              'product_id':{'_user_total_products':'count',
+                            '_user_distinct_products': lambda x: x.nunique()}}
+#us = ka_add_groupby_features_1_vs_n(priors_orders_detail, ['user_id'], agg_dict_3)
+##########################################################################################
+us = pd.concat([
 
-        priors_orders_detail.groupby('user_id')['product_id'].count().rename('_user_total_products'),
-        priors_orders_detail.groupby('user_id')['product_id'].nunique().rename('_user_distinct_products'),
-        (priors_orders_detail.groupby('user_id')['reordered'].sum() /
-            priors_orders_detail[priors_orders_detail['order_number'] > 1].groupby('user_id')['order_number'].count()).rename('_user_reorder_ratio')
-    ], axis=1).reset_index()
-    ############################################################################################
-    users = users.merge(us, how='inner')
+    priors_orders_detail.groupby('user_id')['product_id'].count().rename('_user_total_products'),
+    priors_orders_detail.groupby('user_id')['product_id'].nunique().rename('_user_distinct_products'),
+    (priors_orders_detail.groupby('user_id')['reordered'].sum() /
+        priors_orders_detail[priors_orders_detail['order_number'] > 1].groupby('user_id')['order_number'].count()).rename('_user_reorder_ratio')
+], axis=1).reset_index()
+############################################################################################
+users = users.merge(us, how='inner')
 
-    # 平均每单的商品数
-    # 每单中最多的商品数，最少的商品数++++++++++++++
-    users['_user_average_basket'] = users._user_total_products / users._user_total_orders
+# 平均每单的商品数
+# 每单中最多的商品数，最少的商品数++++++++++++++
+users['_user_average_basket'] = users._user_total_products / users._user_total_orders
 
-    us = orders[orders.eval_set != "prior"][['user_id', 'order_id', 'eval_set', 'days_since_prior_order']]
-    us.rename(index=str, columns={'days_since_prior_order': 'time_since_last_order'}, inplace=True)
+us = orders[orders.eval_set != "prior"][['user_id', 'order_id', 'eval_set', 'days_since_prior_order']]
+us.rename(index=str, columns={'days_since_prior_order': 'time_since_last_order'}, inplace=True)
 
-    users = users.merge(us, how='inner')
+users = users.merge(us, how='inner')
 
-    # Database Part
+# Database Part
 
-    # 这里应该还有很多变量可以被添加
-    # _up_order_count: 用户购买该商品的次数
-    # _up_first_order_number: 用户第一次购买该商品所处的订单数
-    # _up_last_order_number: 用户最后一次购买该商品所处的订单数
-    # _up_average_cart_position: 该商品被添加到购物篮中的平均位置
-    agg_dict_4 = {'order_number':{'_up_order_count': 'count',
-                                  '_up_first_order_number': 'min',
-                                  '_up_last_order_number':'max'},
-                  'add_to_cart_order':{'_up_average_cart_position': 'mean'}}
+# 这里应该还有很多变量可以被添加
+# _up_order_count: 用户购买该商品的次数
+# _up_first_order_number: 用户第一次购买该商品所处的订单数
+# _up_last_order_number: 用户最后一次购买该商品所处的订单数
+# _up_average_cart_position: 该商品被添加到购物篮中的平均位置
+agg_dict_4 = {'order_number':{'_up_order_count': 'count',
+                              '_up_first_order_number': 'min',
+                              '_up_last_order_number':'max'},
+              'add_to_cart_order':{'_up_average_cart_position': 'mean'}}
 
-    data = ka_add_groupby_features_1_vs_n(df=priors_orders_detail,
-                                                          group_columns_list=['user_id', 'product_id'],
-                                                          agg_dict=agg_dict_4)
+data = ka_add_groupby_features_1_vs_n(df=priors_orders_detail,
+                                                      group_columns_list=['user_id', 'product_id'],
+                                                      agg_dict=agg_dict_4)
 
-    data = data.merge(prd, how='inner', on='product_id').merge(users, how='inner', on='user_id')
-    # 该商品购买次数 / 总的订单数
-    # 最近一次购买商品 - 最后一次购买该商品
-    # 该商品购买次数 / 第一次购买该商品到最后一次购买商品的的订单数
-    data['_up_order_rate'] = data._up_order_count / data._user_total_orders
-    data['_up_order_since_last_order'] = data._user_total_orders - data._up_last_order_number
-    data['_up_order_rate_since_first_order'] = data._up_order_count / (data._user_total_orders - data._up_first_order_number + 1)
+data = data.merge(prd, how='inner', on='product_id').merge(users, how='inner', on='user_id')
+# 该商品购买次数 / 总的订单数
+# 最近一次购买商品 - 最后一次购买该商品
+# 该商品购买次数 / 第一次购买该商品到最后一次购买商品的的订单数
+data['_up_order_rate'] = data._up_order_count / data._user_total_orders
+data['_up_order_since_last_order'] = data._user_total_orders - data._up_last_order_number
+data['_up_order_rate_since_first_order'] = data._up_order_count / (data._user_total_orders - data._up_first_order_number + 1)
 
-    # add user_id to train set
-    train = train.merge(right=orders[['order_id', 'user_id']], how='left', on='order_id')
-    data = data.merge(train[['user_id', 'product_id', 'reordered']], on=['user_id', 'product_id'], how='left')
+# add user_id to train set
+train = train.merge(right=orders[['order_id', 'user_id']], how='left', on='order_id')
+data = data.merge(train[['user_id', 'product_id', 'reordered']], on=['user_id', 'product_id'], how='left')
 
-    # release Memory
-    # del train, prd, users
-    # gc.collect()
-    # release Memory
-    del priors_orders_detail, orders
-    gc.collect()
+# release Memory
+# del train, prd, users
+# gc.collect()
+# release Memory
+del priors_orders_detail, orders
+gc.collect()
 
-    data.to_pickle('kernel38-data.pkl')
+data.to_pickle('kernel38-data.pkl')
 ##########################################验证部分##################################################
 '''
 from functools import partial # to reduce df memory consumption by applying to_numeric
