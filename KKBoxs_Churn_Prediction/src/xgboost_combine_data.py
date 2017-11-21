452292b83e054b02cd735809e38d5774d5292ad1
WuZhuoran <munoliver007@gmail.com>
2017-11-21 18:16:08 -0500

    fix: try merge on left and right key

--- KKBoxs_Churn_Prediction/src/xgboost_combine_data.py
+++ KKBoxs_Churn_Prediction/src/xgboost_combine_data.py
@@ -6,7 +6,7 @@ import xgboost as xgb
 transactions = pd.read_csv('../input/transactions_v2.csv')  # reading the transaction file
 members = pd.read_csv('../input/members_v3.csv')
 # user_log = pd.read_csv('../input/user_logs_v2.csv')
-user_log = pd.read_csv('../input/processed_user_log.csv', index_col='msno')
+user_log = pd.read_csv('../input/processed_user_log.csv')
 train = pd.read_csv('../input/train_v2.csv')
 test = pd.read_csv('../input/sample_submission_v2.csv')
 
@@ -170,7 +170,10 @@ newdf_grouped['days_to_buy_membership'] = newdf_grouped['transaction_date'] - ne
 
 newdf_grouped['days_to_buy_membership'] = (newdf_grouped['days_to_buy_membership'] / np.timedelta64(1, 'D')).astype(int)
 
-newdf_grouped = newdf_grouped.merge(user_log, how='inner', left_on='msno', right_on='msno')
+print(newdf_grouped.columns)
+print(user_log.columns)
+
+newdf_grouped = pd.merge(newdf_grouped, user_log, how='outer', on=['msno'])
 
 print('The data column is:')
 print(newdf_grouped.dtypes)
