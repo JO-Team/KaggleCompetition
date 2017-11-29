ddfb3f048d0016eb6ac4c3a7e94a5299dc119005
WuZhuoran <munoliver007@gmail.com>
2017-11-29 18:07:10 -0500

    test: update merge group by

--- KKBoxs_Churn_Prediction/src/merge_test.py
+++ KKBoxs_Churn_Prediction/src/merge_test.py
@@ -1,8 +1,18 @@
 import pandas as pd
 
 user_log_v1 = pd.read_csv('../input/processed_user_log.csv', nrows=1000)
+print(len(user_log_v1))
 user_log_v2 = pd.read_csv('../input/processed_user_log_v2.csv', nrows=1000)
+print(len(user_log_v2))
 
 user_log = user_log_v1.append(user_log_v2, ignore_index=True)
 
-print(user_log.head())
+func = {'date_min': ['min'], 'date_max': ['max'], 'date_count': ['count'],
+        'num_25_sum': ['sum'], 'num_50_sum': ['sum'],
+        'num_75_sum': ['sum'], 'num_985_sum': ['sum'],
+        'num_100_sum': ['sum'], 'num_unq_sum': ['sum'], 'total_secs_sum': ['sum']}
+processed_user_log = user_log.groupby(user_log.index).agg(func)
+print(len(processed_user_log))
+processed_user_log.columns = processed_user_log.columns.get_level_values(0)
+
+processed_user_log.to_csv("../input/processed_user_log_v2.csv", index=False)
