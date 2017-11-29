0e3fce27b268cb6ee5b0c9a7be6d8a0e39c83924
WuZhuoran <munoliver007@gmail.com>
2017-11-29 18:48:01 -0500

    fix: use all log

--- KKBoxs_Churn_Prediction/src/merge_test.py
+++ KKBoxs_Churn_Prediction/src/merge_test.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-user_log_v1 = pd.read_csv('../input/processed_user_log_v1.csv', nrows=1000)
+user_log_v1 = pd.read_csv('../input/processed_user_log_v1.csv')
 print(len(user_log_v1))
-user_log_v2 = pd.read_csv('../input/processed_user_log_v2.csv', nrows=1000)
+user_log_v2 = pd.read_csv('../input/processed_user_log_v2.csv')
 print(len(user_log_v2))
 
 user_log = user_log_v1.append(user_log_v2, ignore_index=True)
@@ -15,4 +15,5 @@ processed_user_log = user_log.groupby("msno").agg(func)
 print(len(processed_user_log))
 # processed_user_log.columns = processed_user_log.columns.get_level_values(0)
 print(processed_user_log.head())
+
 processed_user_log.to_csv("../input/processed_user_log_all.csv")
