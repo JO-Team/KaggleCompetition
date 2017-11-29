940423b597f863916c299692c851d5c8afebcd86
WuZhuoran <munoliver007@gmail.com>
2017-11-29 18:07:56 -0500

    test: use all data

--- KKBoxs_Churn_Prediction/src/merge_test.py
+++ KKBoxs_Churn_Prediction/src/merge_test.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-user_log_v1 = pd.read_csv('../input/processed_user_log.csv', nrows=1000)
+user_log_v1 = pd.read_csv('../input/processed_user_log.csv')
 print(len(user_log_v1))
-user_log_v2 = pd.read_csv('../input/processed_user_log_v2.csv', nrows=1000)
+user_log_v2 = pd.read_csv('../input/processed_user_log_v2.csv')
 print(len(user_log_v2))
 
 user_log = user_log_v1.append(user_log_v2, ignore_index=True)
