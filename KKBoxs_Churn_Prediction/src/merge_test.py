b9972dfd89cecf34b723cbde4821bfbb37a6d6c8
WuZhuoran <munoliver007@gmail.com>
2017-11-29 18:58:36 -0500

    fix: set columns level

--- KKBoxs_Churn_Prediction/src/merge_test.py
+++ KKBoxs_Churn_Prediction/src/merge_test.py
@@ -13,7 +13,7 @@ func = {'date_min': ['min'], 'date_max': ['max'], 'date_count': ['count'],
         'num_100_sum': ['sum'], 'num_unq_sum': ['sum'], 'total_secs_sum': ['sum']}
 processed_user_log = user_log.groupby("msno").agg(func)
 print(len(processed_user_log))
-# processed_user_log.columns = processed_user_log.columns.get_level_values(0)
+processed_user_log.columns = processed_user_log.columns.get_level_values(0)
 print(processed_user_log.head())
 
 processed_user_log.to_csv("../input/processed_user_log_all.csv")
