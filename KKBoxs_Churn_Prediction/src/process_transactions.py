20e0286b0c31094dc49c2bd301504916a74c4771
WuZhuoran <munoliver007@gmail.com>
2017-11-29 17:22:12 -0500

    feat: process transactions

--- KKBoxs_Churn_Prediction/src/process_transactions.py
+++ KKBoxs_Churn_Prediction/src/process_transactions.py
@@ -1,6 +1,5 @@
-import time
-import pandas as pd
 import numpy as np
+import pandas as pd
 
 transactions_v1 = pd.read_csv('../input/transactions.csv')
 transactions_v2 = pd.read_csv('../input/transactions_v2.csv')
@@ -31,38 +30,43 @@ newdf = newdf.rename(columns=payment_method_id)  # renaming the new columns
 del newdf['payment_method_id']  # deleting the extra columns
 
 newdf_grouped = newdf.groupby('msno').agg({
-                                           'payment_plan_days': np.mean, 'plan_list_price': np.mean,
-                                           'actual_amount_paid': np.mean,
-                                           'is_auto_renew': np.mean, 'transaction_date': min,
-                                           'membership_expire_date': max,
-                                           'is_cancel': np.mean,
-                                           'payment_method_id2': np.mean,
-                                           'payment_method_id3': sum,
-                                           'payment_method_id4': np.sum,
-                                           'payment_method_id5': np.sum,
-                                           'payment_method_id6': np.sum,
-                                           'payment_method_id8': np.sum,
-                                           'payment_method_id10': np.sum,
-                                           'payment_method_id11': np.sum, 'payment_method_id12': np.sum,
-                                           'payment_method_id13': np.sum,
-                                           'payment_method_id14': np.sum, 'payment_method_id15': np.sum,
-                                           'payment_method_id16': np.sum,
-                                           'payment_method_id17': np.sum, 'payment_method_id18': np.sum,
-                                           'payment_method_id19': np.sum,
-                                           'payment_method_id20': np.sum,
-                                           'payment_method_id21': np.sum,
-                                           'payment_method_id22': np.sum,
-                                           'payment_method_id23': np.sum,
-                                           'payment_method_id24': np.sum,
-                                           'payment_method_id25': np.sum,
-                                           'payment_method_id26': np.sum, 'payment_method_id27': np.sum,
-                                           'payment_method_id28': np.sum,
-                                           'payment_method_id29': np.sum, 'payment_method_id30': np.sum,
-                                           'payment_method_id31': np.sum,
-                                           'payment_method_id32': np.sum, 'payment_method_id33': np.sum,
-                                           'payment_method_id34': np.sum,
-                                           'payment_method_id35': np.sum, 'payment_method_id36': np.sum,
-                                           'payment_method_id37': np.sum,
-                                           'payment_method_id38': np.sum, 'payment_method_id39': np.sum,
-                                           'payment_method_id40': np.sum,
-                                           'payment_method_id41': np.sum})
+    'payment_plan_days': np.mean, 'plan_list_price': np.mean,
+    'actual_amount_paid': np.mean,
+    'is_auto_renew': np.mean, 'transaction_date': min,
+    'membership_expire_date': max,
+    'is_cancel': np.mean,
+    'payment_method_id2': np.mean,
+    'payment_method_id3': sum,
+    'payment_method_id4': np.sum,
+    'payment_method_id5': np.sum,
+    'payment_method_id6': np.sum,
+    'payment_method_id8': np.sum,
+    'payment_method_id10': np.sum,
+    'payment_method_id11': np.sum, 'payment_method_id12': np.sum,
+    'payment_method_id13': np.sum,
+    'payment_method_id14': np.sum, 'payment_method_id15': np.sum,
+    'payment_method_id16': np.sum,
+    'payment_method_id17': np.sum, 'payment_method_id18': np.sum,
+    'payment_method_id19': np.sum,
+    'payment_method_id20': np.sum,
+    'payment_method_id21': np.sum,
+    'payment_method_id22': np.sum,
+    'payment_method_id23': np.sum,
+    'payment_method_id24': np.sum,
+    'payment_method_id25': np.sum,
+    'payment_method_id26': np.sum, 'payment_method_id27': np.sum,
+    'payment_method_id28': np.sum,
+    'payment_method_id29': np.sum, 'payment_method_id30': np.sum,
+    'payment_method_id31': np.sum,
+    'payment_method_id32': np.sum, 'payment_method_id33': np.sum,
+    'payment_method_id34': np.sum,
+    'payment_method_id35': np.sum, 'payment_method_id36': np.sum,
+    'payment_method_id37': np.sum,
+    'payment_method_id38': np.sum, 'payment_method_id39': np.sum,
+    'payment_method_id40': np.sum,
+    'payment_method_id41': np.sum})
+
+print(newdf_grouped.head())
+print(len(newdf_grouped))
+
+newdf_grouped.to_csv('../input/processed_transaction_all.csv')
