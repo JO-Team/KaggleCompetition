021431b3b9656977a2a4fccdb55294367c1a74b6
WuZhuoran <munoliver007@gmail.com>
2017-11-29 17:43:36 -0500

    feat: update new baseline

--- KKBoxs_Churn_Prediction/src/xgboost_merge_all.py
+++ KKBoxs_Churn_Prediction/src/xgboost_merge_all.py
@@ -1,22 +1,74 @@
-import numpy as np
-import pandas as pd
+import gc
 
-transactions_v1 = pd.read_csv('../input/transactions.csv', nrows=1000)
-transactions_v2 = pd.read_csv('../input/transactions_v2.csv', nrows=1000)
+import pandas as pd
+import sklearn
+import xgboost as xgb
+
+
+def xgb_score(preds, dtrain):
+    labels = dtrain.get_label()
+    return 'log_loss', sklearn.metrics.log_loss(labels, preds)
+
+
+gc.enable()
+
+transactions = pd.read_csv('../input/processed_transaction_all.csv', nrows=1000)
 
 members_v1 = pd.read_csv('../input/members.csv', nrows=1000)
-members_v2 = pd.read_csv('../input/members.csv', nrows=1000)
+members_v2 = pd.read_csv('../input/members_v2.csv', nrows=1000)
+members = members_v1.append(members_v2, ignore_index=True)
 
-user_log_v1 = pd.read_csv('../input/processed_user_log.csv', nrows=1000)
-user_log_v2 = pd.read_csv('../input/processed_user_log_v2.csv', nrows=1000)
-
+user_log = pd.read_csv('../input/processed_user_log_all.csv', nrows=1000)
 
 train_v1 = pd.read_csv('../input/train.csv', nrows=1000)
 train_v2 = pd.read_csv('../input/train_v2.csv', nrows=1000)
+train = train_v1.append(train_v2, ignore_index=True)
 
 test = pd.read_csv('../input/sample_submission_v2.csv', nrows=1000)
 
-transactions = transactions_v1.append(transactions_v2)
-members = members_v1.append(members_v2)
-user_log = user_log_v1.append(user_log_v2)
-train = train_v1.append(train_v2)
+# Merge Data
+
+train = pd.merge(train, transactions, how='left', on='msno')
+test = pd.merge(test, transactions, how='left', on='msno')
+
+train = pd.merge(train, user_log, how='left', on='msno')
+test = pd.merge(test, user_log, how='left', on='msno')
+
+train = pd.merge(train, members, how='left', on='msno')
+test = pd.merge(test, members, how='left', on='msno')
+
+train = pd.merge(train, members, how='left', on='msno')
+test = pd.merge(test, members, how='left', on='msno')
+
+gender = {'male': 1, 'female': 2}
+train['gender'] = train['gender'].map(gender)
+test['gender'] = test['gender'].map(gender)
+
+train = train.fillna(0)
+test = test.fillna(0)
+
+cols = [c for c in train.columns if c not in ['is_churn', 'msno']]
+
+fold = 1
+for i in range(fold):
+    params = {
+        'eta': 0.02,  # use 0.002
+        'max_depth': 7,
+        'objective': 'binary:logistic',
+        'eval_metric': 'logloss',
+        'seed': i,
+        'silent': True
+    }
+    x1, x2, y1, y2 = sklearn.model_selection.train_test_split(train[cols], train['is_churn'], test_size=0.3,
+                                                              random_state=i)
+    watchlist = [(xgb.DMatrix(x1, y1), 'train'), (xgb.DMatrix(x2, y2), 'valid')]
+    model = xgb.train(params, xgb.DMatrix(x1, y1), 1500, watchlist, feval=xgb_score, maximize=False, verbose_eval=50,
+                      early_stopping_rounds=50)  # use 1500
+    if i != 0:
+        pred += model.predict(xgb.DMatrix(test[cols]), ntree_limit=model.best_ntree_limit)
+    else:
+        pred = model.predict(xgb.DMatrix(test[cols]), ntree_limit=model.best_ntree_limit)
+pred /= fold
+
+test['is_churn'] = pred.clip(0.0000001, 0.999999)
+test[['msno', 'is_churn']].to_csv('submission_xgboost_baseline_merge_data.csv', index=False)
