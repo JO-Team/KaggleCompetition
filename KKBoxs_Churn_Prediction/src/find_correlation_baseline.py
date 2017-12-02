1f3a280397222bae839ad1f1c7b47ec33a5bc52a
WuZhuoran <munoliver007@gmail.com>
2017-12-01 20:02:03 -0500

    feat: update correlation

---
+++ KKBoxs_Churn_Prediction/src/find_correlation_baseline.py
@@ -0,0 +1,68 @@
+import gc
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+
+gc.enable()
+
+transactions = pd.read_csv('../input/processed_transaction_all.csv')
+
+members_v1 = pd.read_csv('../input/members.csv')
+members_v2 = pd.read_csv('../input/members_v2.csv')
+members = members_v1.append(members_v2, ignore_index=True)
+
+user_log = pd.read_csv('../input/processed_user_log_all.csv')
+
+train_v1 = pd.read_csv('../input/train.csv')
+train_v2 = pd.read_csv('../input/train_v2.csv')
+train = train_v1.append(train_v2, ignore_index=True)
+
+test = pd.read_csv('../input/sample_submission_v2.csv')
+
+# Merge Data
+
+train = pd.merge(train, transactions, how='left', on='msno')
+test = pd.merge(test, transactions, how='left', on='msno')
+
+train = pd.merge(train, user_log, how='left', on='msno')
+test = pd.merge(test, user_log, how='left', on='msno')
+
+train = pd.merge(train, members, how='left', on='msno')
+test = pd.merge(test, members, how='left', on='msno')
+
+# Drop duplicates first
+test = test.drop_duplicates('msno')
+
+gender = {'male': 1, 'female': 2}
+train['gender'] = train['gender'].map(gender)
+test['gender'] = test['gender'].map(gender)
+
+train = train.fillna(0)
+test = test.fillna(0)
+
+# Delete date for now
+train = train.drop(['transaction_date', 'membership_expire_date', 'expiration_date', 'registration_init_time'], axis=1)
+test = test.drop(['transaction_date', 'membership_expire_date', 'expiration_date', 'registration_init_time'], axis=1)
+
+corr = train.corr()
+
+print('Train Data Set Correlation:')
+print(corr)
+
+# Generate a mask for the upper triangle
+mask = np.zeros_like(corr, dtype=np.bool)
+mask[np.triu_indices_from(mask)] = True
+
+# Set up the matplotlib figure
+f, ax = plt.subplots(figsize=(11, 9))
+
+# Generate a custom diverging colormap
+cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+# Draw the heatmap with the mask and correct aspect ratio
+headmap = sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+                      square=True, linewidths=.5, cbar_kws={"shrink": .5})
+fig = headmap.get_figure()
+fig.savefig('Features_Correlation_Heatmap_Baseline')
