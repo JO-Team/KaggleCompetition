1d544efd7305fcf1ff45485de4cd605df1615fa3
WuZhuoran <munoliver007@gmail.com>
2017-12-11 15:51:36 -0500

    test: print

--- KKBoxs_Churn_Prediction/src/process_features_part.py
+++ KKBoxs_Churn_Prediction/src/process_features_part.py
@@ -256,11 +256,11 @@ for i in range(10):
         print("Loop ", i, "took %s seconds" % (time.time() - start_time))
     del user_log_chunk
 
-train_final = process_train_user_log(train_final, 0)
-train_final.columns = train_final.columns.get_level_values(0)
-
 print(len(train_final))
 print(train_final.columns)
+
+train_final = process_train_user_log(train_final, 0)
+train_final.columns = train_final.columns.get_level_values(0)
 
 # train_final.to_csv("../input/processed_features_train_final_v1.csv", index=False)
 
@@ -279,11 +279,11 @@ for i in range(10):
         print("Loop ", i, "took %s seconds" % (time.time() - start_time))
     del user_log_chunk
 
-test_final = process_train_user_log(test_final, 1)
-test_final.columns = test_final.columns.get_level_values(0)
-
 print(len(test_final))
 print(test_final.columns)
+
+test_final = process_train_user_log(test_final, 1)
+test_final.columns = test_final.columns.get_level_values(0)
 
 # test_final.to_csv("../input/processed_features_test_final_v1.csv", index=False)
 
