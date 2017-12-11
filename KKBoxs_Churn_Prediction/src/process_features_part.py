dbaba091842f611894ab5356853488d2b2c30eb0
WuZhuoran <munoliver007@gmail.com>
2017-12-10 20:04:42 -0500

    test

--- KKBoxs_Churn_Prediction/src/process_features_part.py
+++ KKBoxs_Churn_Prediction/src/process_features_part.py
@@ -13,8 +13,8 @@ def process_train_user_log(train):
     # 用户一个月的活跃角度
     # 一个月的登陆天数
     train_log_day = train.groupby(['msno']).date.agg({'log_day': 'count'}).reset_index()
-    train_log_day.columns = train_log_day.columns.get_level_values(0)
-    print(train_log_day.head(5))
+    print(train.columns)
+    print(train_log_day.columns)
     train = pd.merge(train, train_log_day, on=['msno'], how='left')
 
     # 一个月的听歌汇总
