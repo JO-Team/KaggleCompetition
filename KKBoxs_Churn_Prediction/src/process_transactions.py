5efdbee087de535104ce44b29a333698e0319ab3
WuZhuoran <munoliver007@gmail.com>
2017-12-11 21:49:55 -0500

    feat: add more transaction features

--- KKBoxs_Churn_Prediction/src/process_transactions.py
+++ KKBoxs_Churn_Prediction/src/process_transactions.py
@@ -1,8 +1,8 @@
 import numpy as np
 import pandas as pd
 
-transactions_v1 = pd.read_csv('../input/transactions.csv', nrows=100)
-transactions_v2 = pd.read_csv('../input/transactions_v2.csv', nrows=100)
+transactions_v1 = pd.read_csv('../input/transactions.csv')
+transactions_v2 = pd.read_csv('../input/transactions_v2.csv')
 
 transactions = transactions_v1.append(transactions_v2, ignore_index=True)
 
@@ -34,9 +34,7 @@ newdf_grouped = newdf.groupby('msno').agg({
     'actual_amount_paid': np.mean,
     'is_auto_renew': np.mean, 'transaction_date': min,
     'membership_expire_date': max,
-    'is_cancel': np.mean})
-
-'''
+    'is_cancel': np.mean,
     'payment_method_id2': np.mean,
     'payment_method_id3': np.sum,
     'payment_method_id4': np.sum,
@@ -66,31 +64,30 @@ newdf_grouped = newdf.groupby('msno').agg({
     'payment_method_id37': np.sum,
     'payment_method_id38': np.sum, 'payment_method_id39': np.sum,
     'payment_method_id40': np.sum,
-    'payment_method_id41': np.sum
-'''
+    'payment_method_id41': np.sum})
 
-#################################################################################################################
+# ################################################################################################################
 
 newdf_grouped = newdf_grouped.sort_values(by=['transaction_date'], ascending=[False]).reset_index()
-print(newdf_grouped.head())
 newdf_grouped = newdf_grouped.drop_duplicates(subset=['msno'], keep='first')
 
-#discount
+# discount
 newdf_grouped['discount'] = newdf_grouped['plan_list_price'] - newdf_grouped['actual_amount_paid']
-#amt_per_day
+# amt_per_day
 newdf_grouped['amt_per_day'] = newdf_grouped['actual_amount_paid'] / newdf_grouped['payment_plan_days']
-#is_discount
+# is_discount
 newdf_grouped['is_discount'] = newdf_grouped.discount.apply(lambda x: 1 if x > 0 else 0)
-#membership_duration
+# membership_duration
 newdf_grouped['membership_days'] = pd.to_datetime(newdf_grouped['membership_expire_date']).subtract(pd.to_datetime(
-        newdf_grouped['transaction_date'])).dt.days.astype(int)
+    newdf_grouped['transaction_date'])).dt.days.astype(int)
 
 newdf_grouped['transaction_date_year'] = newdf_grouped['transaction_date'].apply(lambda x: int(str(x)[:4]))
 newdf_grouped['transaction_date_month'] = newdf_grouped['transaction_date'].apply(lambda x: int(str(x)[4:6]))
 newdf_grouped['transaction_date_day'] = newdf_grouped['transaction_date'].apply(lambda x: int(str(x)[-2:]))
 
 newdf_grouped['membership_expire_date_year'] = newdf_grouped['membership_expire_date'].apply(lambda x: int(str(x)[:4]))
-newdf_grouped['membership_expire_date_month'] = newdf_grouped['membership_expire_date'].apply(lambda x: int(str(x)[4:6]))
+newdf_grouped['membership_expire_date_month'] = newdf_grouped['membership_expire_date'].apply(
+    lambda x: int(str(x)[4:6]))
 newdf_grouped['membership_expire_date_day'] = newdf_grouped['membership_expire_date'].apply(lambda x: int(str(x)[-2:]))
 
 newdf_grouped['transaction_date_year'] = newdf_grouped['transaction_date_year'].astype(np.int16)
@@ -105,4 +102,4 @@ newdf_grouped['membership_expire_date_day'] = newdf_grouped['membership_expire_d
 print(newdf_grouped.head())
 print(len(newdf_grouped))
 
-# newdf_grouped.to_csv('../input/processed_transaction_features.csv')
+newdf_grouped.to_csv('../input/processed_transaction_features.csv')
