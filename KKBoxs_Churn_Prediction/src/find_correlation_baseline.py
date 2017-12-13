68c7fdce672effdb00b9f02fa5bd539f171255e7
WuZhuoran <munoliver007@gmail.com>
2017-12-13 14:23:35 -0500

    feat: add analysis correaltion

--- KKBoxs_Churn_Prediction/src/find_correlation_baseline.py
+++ KKBoxs_Churn_Prediction/src/find_correlation_baseline.py
@@ -7,13 +7,14 @@ import seaborn as sns
 
 gc.enable()
 
-transactions = pd.read_csv('../input/processed_transaction_all.csv')
+# transactions = pd.read_csv('../input/processed_transaction_all.csv')
 
 members_v1 = pd.read_csv('../input/members.csv')
 members_v2 = pd.read_csv('../input/members_v2.csv')
 members = members_v1.append(members_v2, ignore_index=True)
 
-user_log = pd.read_csv('../input/processed_user_log_all.csv')
+user_log_train = pd.read_csv('../input/processed_features_user_log_feb.csv')
+user_log_test = pd.read_csv('../input/processed_features_user_log_mar.csv')
 
 train_v1 = pd.read_csv('../input/train.csv')
 train_v2 = pd.read_csv('../input/train_v2.csv')
@@ -23,11 +24,11 @@ test = pd.read_csv('../input/sample_submission_v2.csv')
 
 # Merge Data
 
-train = pd.merge(train, transactions, how='left', on='msno')
-test = pd.merge(test, transactions, how='left', on='msno')
+# train = pd.merge(train, transactions, how='left', on='msno')
+# test = pd.merge(test, transactions, how='left', on='msno')
 
-train = pd.merge(train, user_log, how='left', on='msno')
-test = pd.merge(test, user_log, how='left', on='msno')
+train = pd.merge(train, user_log_train, how='left', on='msno')
+test = pd.merge(test, user_log_test, how='left', on='msno')
 
 train = pd.merge(train, members, how='left', on='msno')
 test = pd.merge(test, members, how='left', on='msno')
@@ -48,10 +49,10 @@ test = test.drop(['transaction_date', 'membership_expire_date', 'expiration_date
 
 corr = train.corr()
 
-print('Train Data Set Correlation:')
-print(corr)
+# print('Train Data Set Correlation:')
+# print(corr)
 
-corr.to_csv('baseline_corr.csv', index=False)
+corr.to_csv('user_log_features_without_transaction_corr.csv', index=False)
 
 # Generate a mask for the upper triangle
 mask = np.zeros_like(corr, dtype=np.bool)
@@ -67,4 +68,4 @@ cmap = sns.diverging_palette(220, 10, as_cmap=True)
 headmap = sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
                       square=True, linewidths=.5, cbar_kws={"shrink": .5})
 fig = headmap.get_figure()
-fig.savefig('Features_Correlation_Heatmap_Baseline')
+fig.savefig('Features_Correlation_Heatmap_user_log')
