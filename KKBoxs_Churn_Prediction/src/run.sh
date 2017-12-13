871d14999dd67c0998a12560a4cd552c9fa4e6b9
WuZhuoran <munoliver007@gmail.com>
2017-12-12 21:16:33 -0500

    feat: drop useless columns features

--- KKBoxs_Churn_Prediction/src/run.sh
+++ KKBoxs_Churn_Prediction/src/run.sh
@@ -1,5 +1,5 @@
 # Dec 8, 2017
-python3 process_user_log_feb.py
-python3 process_user_log_mar.py
+# python3 process_user_log_feb.py
+# python3 process_user_log_mar.py
 python3 process_features_user_log.py
 python3 xgboost_features.py
