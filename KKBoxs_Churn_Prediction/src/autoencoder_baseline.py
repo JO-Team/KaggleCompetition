a217fe8ceaf3a13333dc5ae88f14bcd4432cbde5
WuZhuoran <munoliver007@gmail.com>
2017-12-14 19:09:38 -0500

    feat: add frac random sample

--- KKBoxs_Churn_Prediction/src/autoencoder_baseline.py
+++ KKBoxs_Churn_Prediction/src/autoencoder_baseline.py
@@ -16,6 +16,7 @@ members = members_v1.append(members_v2, ignore_index=True)
 
 user_log_train = pd.read_csv('../input/processed_features_user_log_feb.csv')
 user_log_test = pd.read_csv('../input/processed_features_user_log_mar.csv')
+user_log_all = pd.read_csv('../input/processed_user_log_all.csv')
 
 train_v1 = pd.read_csv('../input/train.csv')
 train_v2 = pd.read_csv('../input/train_v2.csv')
@@ -31,11 +32,17 @@ test = pd.merge(test, transactions, how='left', on='msno')
 train = pd.merge(train, user_log_train, how='left', on='msno')
 test = pd.merge(test, user_log_test, how='left', on='msno')
 
+train = pd.merge(train, user_log_all, how='left', on='msno')
+test = pd.merge(test, user_log_all, how='left', on='msno')
+
 train = pd.merge(train, members, how='left', on='msno')
 test = pd.merge(test, members, how='left', on='msno')
 
 # Drop duplicates first
 test = test.drop_duplicates('msno')
+
+del transactions, members_v1, members_v2, members, user_log_train, user_log_test, user_log_all, train_v1, train_v2
+gc.collect()
 
 gender = {'male': 1, 'female': 2}
 train['gender'] = train['gender'].map(gender)
@@ -54,19 +61,24 @@ cols = [c for c in train.columns if c not in ['is_churn', 'msno']]
 # train['is_churn'] = keras.utils.to_categorical(train['is_churn'], num_classes=2)
 
 train_0 = train[train['is_churn'] == 0]
-
-print(len(train_0))
-
 train_1 = train[train['is_churn'] == 1]
+
+# Enlarge train_1 for 17 times
+'''
 train_append = train_1
 
 for _ in range(17):
     train_append = train_append.append(train_1)
 
-print(len(train_1))
-print(len(train_append))
-
 train = train_0.append(train_append)
+'''
+
+# train1 random sample 1/17
+print(len(train_0))
+train_0.sample(frac=1.0/17.0, replace=True, random_state=47)
+print(len(train_0))
+train = train_0.append(train_1)
+print(train)
 
 X_train, X_test = train_test_split(train, test_size=0.2, random_state=47, shuffle=True)
 y_train = X_train['is_churn']
@@ -79,12 +91,12 @@ X_train = X_train.values
 X_test = X_test.values
 
 input_dim = X_train.shape[1]
-encoding_dim = 14
 
 autoencoder = Sequential()
-autoencoder.add(Dense(62, input_dim=62))
-autoencoder.add(Dense(31, activation='relu'))
-autoencoder.add(Dense(15, activation='relu'))
+autoencoder.add(Dense(input_dim, input_dim=input_dim))
+autoencoder.add(Dense(int(input_dim / 2), activation='relu'))
+autoencoder.add(Dense(int(input_dim / 2), activation='relu'))
+autoencoder.add(Dense(int(input_dim / 2), activation='relu'))
 autoencoder.add(Dense(1, activation='sigmoid'))
 
 autoencoder.summary()
