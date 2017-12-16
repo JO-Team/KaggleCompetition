e850f3ca042824c244397aadbe5ff62fad896447
WuZhuoran <munoliver007@gmail.com>
2017-12-15 19:01:02 -0500

    feat: add xgboost features selection

--- KKBoxs_Churn_Prediction/src/xgboost_features.py
+++ KKBoxs_Churn_Prediction/src/xgboost_features.py
@@ -1,5 +1,6 @@
 import gc
 
+import numpy as np
 import pandas as pd
 import sklearn
 import xgboost as xgb
@@ -12,22 +13,30 @@ def xgb_score(preds, dtrain):
 
 gc.enable()
 
-transactions = pd.read_csv('../input/processed_transaction_all.csv')
+transactions_train = pd.read_csv('../input/processed_transaction_features_feb.csv', index_col=0)
+transactions_test = pd.read_csv('../input/processed_transaction_features_mar.csv', index_col=0)
+transactions = pd.read_csv('../input/processed_transaction_features.csv', index_col=0)
 
-members_v1 = pd.read_csv('../input/members.csv')
-members_v2 = pd.read_csv('../input/members_v2.csv')
-members = members_v1.append(members_v2, ignore_index=True)
+transactions = transactions[
+    ['msno', 'discount', 'amt_per_day', 'is_discount', 'membership_days', 'transaction_date_year',
+     'transaction_date_month',
+     'transaction_date_day', 'membership_expire_date_year', 'membership_expire_date_month',
+     'membership_expire_date_day']]
+
+members = pd.read_csv('../input/members_v3.csv')
 
 user_log_train = pd.read_csv('../input/processed_features_user_log_feb.csv')
 user_log_test = pd.read_csv('../input/processed_features_user_log_mar.csv')
+user_log_all = pd.read_csv('../input/processed_user_log_all.csv')
 
-train_v1 = pd.read_csv('../input/train.csv')
-train_v2 = pd.read_csv('../input/train_v2.csv')
-train = train_v1.append(train_v2, ignore_index=True)
+train = pd.read_csv('../input/train_v2.csv')
 
 test = pd.read_csv('../input/sample_submission_v2.csv')
 
 # Merge Data
+
+train = pd.merge(train, transactions_train, how='left', on='msno')
+test = pd.merge(test, transactions_test, how='left', on='msno')
 
 train = pd.merge(train, transactions, how='left', on='msno')
 test = pd.merge(test, transactions, how='left', on='msno')
@@ -35,8 +44,14 @@ test = pd.merge(test, transactions, how='left', on='msno')
 train = pd.merge(train, user_log_train, how='left', on='msno')
 test = pd.merge(test, user_log_test, how='left', on='msno')
 
+train = pd.merge(train, user_log_all, how='left', on='msno')
+test = pd.merge(test, user_log_all, how='left', on='msno')
+
 train = pd.merge(train, members, how='left', on='msno')
 test = pd.merge(test, members, how='left', on='msno')
+
+del transactions, members, user_log_train, user_log_test
+gc.collect()
 
 # Drop duplicates first
 test = test.drop_duplicates('msno')
@@ -45,13 +60,74 @@ gender = {'male': 1, 'female': 2}
 train['gender'] = train['gender'].map(gender)
 test['gender'] = test['gender'].map(gender)
 
+train['bd'] = train['bd'].replace(0, train['bd'].mode())
+test['bd'] = test['bd'].replace(0, test['bd'].mode())
+
+train['gender'] = train['gender'].replace(0, train['gender'].mean())
+test['gender'] = test['gender'].replace(0, test['gender'].mean())
+
 train = train.fillna(0)
 test = test.fillna(0)
 
 # Delete date for now
-train = train.drop(['transaction_date', 'membership_expire_date', 'expiration_date', 'registration_init_time'], axis=1)
-test = test.drop(['transaction_date', 'membership_expire_date', 'expiration_date', 'registration_init_time'], axis=1)
-# Delete date for now
+train = train.drop(['transaction_date', 'membership_expire_date', 'registration_init_time'], axis=1)
+test = test.drop(['transaction_date', 'membership_expire_date', 'registration_init_time'], axis=1)
+
+# Remove Features with 0 feature importance
+train = train.drop(
+    ['payment_method_id14',
+     'payment_method_id18',
+     'payment_method_id21',
+     'payment_method_id26',
+     'payment_method_id35',
+     'transaction_date_month_x',
+     'transaction_date_day_x',
+     'membership_expire_date_year_x',
+     'membership_expire_date_month_x',
+     'membership_expire_date_day_x',
+     'transaction_date_day_y',
+     'membership_expire_date_day_y'], axis=1)
+test = test.drop(
+    ['payment_method_id14',
+     'payment_method_id18',
+     'payment_method_id21',
+     'payment_method_id26',
+     'payment_method_id35',
+     'transaction_date_month_x',
+     'transaction_date_day_x',
+     'membership_expire_date_year_x',
+     'membership_expire_date_month_x',
+     'membership_expire_date_day_x',
+     'transaction_date_day_y',
+     'membership_expire_date_day_y'], axis=1)
+
+# Remove Features with feature importance less than 100
+train = train.drop(
+    ['payment_method_id16',
+     'payment_method_id17',
+     'payment_method_id19',
+     'payment_method_id23',
+     'payment_method_id27',
+     'payment_method_id28',
+     'payment_method_id31',
+     'is_discount_x',
+     'transaction_date_year_x'], axis=1)
+test = test.drop(
+    ['payment_method_id16',
+     'payment_method_id17',
+     'payment_method_id19',
+     'payment_method_id23',
+     'payment_method_id27',
+     'payment_method_id28',
+     'payment_method_id31',
+     'is_discount_x',
+     'transaction_date_year_x'], axis=1)
+
+train['autorenew_&_not_cancel'] = ((train.is_auto_renew == 1) == (train.is_cancel == 0)).astype(np.int8)
+test['autorenew_&_not_cancel'] = ((test.is_auto_renew == 1) == (test.is_cancel == 0)).astype(np.int8)
+
+train['notAutorenew_&_cancel'] = ((train.is_auto_renew == 0) == (train.is_cancel == 1)).astype(np.int8)
+test['notAutorenew_&_cancel'] = ((test.is_auto_renew == 0) == (test.is_cancel == 1)).astype(np.int8)
 
 feature_list = [
     # raw data
@@ -95,7 +171,7 @@ params = {
 x1, x2, y1, y2 = sklearn.model_selection.train_test_split(train[cols], train['is_churn'], test_size=0.3,
                                                           random_state=2017)
 watchlist = [(xgb.DMatrix(x1, y1), 'train'), (xgb.DMatrix(x2, y2), 'valid')]
-cv_output = xgb.cv(params, xgb.DMatrix(x1, y1), num_boost_round=2500, early_stopping_rounds=20, verbose_eval=50, show_stdv=False)
+# cv_output = xgb.cv(params, xgb.DMatrix(x1, y1), num_boost_round=2500, early_stopping_rounds=20, verbose_eval=50, show_stdv=False)
 model = xgb.train(params, xgb.DMatrix(x1, y1), 2500, watchlist, feval=xgb_score, maximize=False, verbose_eval=50,
                   early_stopping_rounds=50)
 
@@ -103,5 +179,5 @@ pred = model.predict(xgb.DMatrix(test[cols]), ntree_limit=model.best_ntree_limit
 
 test['is_churn'] = pred.clip(0.0000001, 0.999999)
 print(len(test))
-test[['msno', 'is_churn']].to_csv('submission_xgboost_userlog_transaction_features_eta_0.002_round_2500_Dec_11.csv',
+test[['msno', 'is_churn']].to_csv('submission_xgboost_features_selection_eta_0.002_round_2500_Dec_15.csv',
                                   index=False)
