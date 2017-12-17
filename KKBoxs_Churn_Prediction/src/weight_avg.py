67f490b354d5adee8e483373f0c90db9f6f3c6d7
WuZhuoran <munoliver007@gmail.com>
2017-12-17 13:46:44 -0500

    avg 0.44

--- KKBoxs_Churn_Prediction/src/weight_avg.py
+++ KKBoxs_Churn_Prediction/src/weight_avg.py
@@ -25,17 +25,17 @@ file1.to_csv('submission_weight_avg_4_0.3_0.3_0.2_0.2.csv', index=False)
 
 # LB 0.12432 CV 0.122651 Train LogLoss 0.103781
 file1 = pd.read_csv('result/submission_lightgbm_all_time_feaetures_origin_version_eta_0.002_round_2500_Dec_16.csv')
-weight1 = 0.3
+weight1 = 0.28
 
 # LB 0.12383 CV 0.127227
 file2 = pd.read_csv('result/submission_lightgbm_features_trans_user_log_split_by_month_eta_0.002_round_2500_Dec_15.csv')
-weight2 = 0.3
+weight2 = 0.28
 
 # LB 0.12393 CV 0.122639 Train LogLoss 0.102916
 file3 = pd.read_csv('result/submission_lightgbm_features_selection_origin_version_eta_0.002_round_2500_Dec_17.csv')
-weight3 = 0.4
+weight3 = 0.44
 
 file1['is_churn'] = file1['is_churn'] * weight1 + file2['is_churn'] * weight2 + \
                     file3['is_churn'] * weight3
 
-file1.to_csv('submission_weight_avg_0.4_0.3_0.3.csv', index=False)
+file1.to_csv('submission_weight_avg_0.44_0.28_0.28.csv', index=False)
